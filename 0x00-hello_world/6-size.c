0e904f582c295831df7f1140d61f906225bd98dd
uty123 <queenzyuty@yahoo.com>
2022-12-11 06:25:58 -0800

    dsw

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,13 @@ int main (void)
 	int a;
 	char b;
 	float c;
+	long int d;
+	long long int e;
 
 	printf("The size of a int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("The size of a char: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("The size of a float: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof
 	return (0);
 }
