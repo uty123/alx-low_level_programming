ca4acc994f1f1b2e7b2dd1cccab832af99907e32
uty123 <queenzyuty@yahoo.com>
2022-12-11 05:48:50 -0800

    sda

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Return: - Always 0 (Success)
+ */
+int main (void)
+{
+	int a;
+	char b;
+	float c;
+
+	printf("Size of int: %lu byte(s)\n" (unsigned long)sizeof(a));
+	printf("Size of char: %lu byte(s)\n" (unsigned long)sizeof(b));
+	printf("size of float: %lu byte(s)\n" (unsigned long)sizeof(c));
+	return (0);
+}
