a40f13203d263082149f98cc78bb921ce39e948b
uty123 <queenzyuty@yahoo.com>
2022-12-12 00:27:02 -0800

    edjw

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints to the standard error
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
