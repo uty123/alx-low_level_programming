18b17889a0fdbd4746fa9ab895f76efc217b3052
uty123 <queenzyuty@yahoo.com>
2022-12-13 00:37:31 -0800

    dasf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,12 +12,16 @@ int main (void)
 	char b;
 	float c;
 	double d;
+	long int e;
+	long long int f;
 
-	printf("The size of a int =%lu byte(s)\n",sizeof(a));
-	printf("The size of a char =%lu byte(s)\n",sizeof(b));
-	printf("The size of a float =%lu byte(s)\n",sizeof(c));
-	printf("Size of  double =%lu byte(s)\n",sizeof(d));
-	printf("Size of void =%lu byte(s)\n",sizeof(void));
+	printf("The size of a int =%lu byte(s)\n", sizeof(a));
+	printf("The size of a char =%lu byte(s)\n", sizeof(b));
+	printf("The size of a float =%lu byte(s)\n", sizeof(c));
+	printf("Size of  double =%lu byte(s)\n", sizeof(d));
+	printf("Size of void =%lu byte(s)\n", sizeof(void));
+	printf("size of long int =%lu bytes\n", sizeof(e));
+	printf("size of long long int =%lu bytes\n", sizeof(f));
 
 	return (0);
 }
