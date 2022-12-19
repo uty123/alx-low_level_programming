050fef469b8e86125accaa34f97dd4b1708be7f0
uty123 <queenzyuty@yahoo.com>
2022-12-19 03:33:33 -0800

    ghdf

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - Updates the value it points to 98
+ *
+ *@n: is an int whose value is a pointer
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
