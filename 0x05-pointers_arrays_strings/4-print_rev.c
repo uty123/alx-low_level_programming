0f87c58ddd2c778a49a105f0a0e73d252b470476
uty123 <queenzyuty@yahoo.com>
2022-12-20 21:44:48 -0800

    dff

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * print_rev - Prints a string in reverse
+  * @s: The string to print
+  *
+  * Return: void
+  */
+void print_rev(char *s)
+{	int x = 0;
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+	for (x -= 1; x >= 0; x--)
+	{
+		_putchar(s[x]);
+	}
+	_putchar('\n');
+}
